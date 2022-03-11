39046645d9004bea14d3713d509b05e1b9f004f2
Vladimir Banduristov <vaban@norvikbank.ru>
2022-03-11 10:18:02 +0300

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import {terser} from "rollup-plugin-terser";
+import pkg from "./package.json";
+
+export default [
+    {
+        input: "src/index.js",
+        output: {
+            name: "utmAska",
+            file: pkg.browser,
+            format: "umd",
+            esModule: false,
+            exports: "named",
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            babel({
+                exclude: ["node_modules/**"],
+            }),
+            terser({
+                format: {
+                    comments: false
+                },
+                compress: true
+            }),
+        ],
+    },
+    {
+        input: "src/index.js",
+        output: [
+            {
+                file: pkg.module, format: "esm",
+                exports: "auto"
+            },
+            {
+                file: pkg.main, format: "cjs",
+                exports: "auto"
+            },
+        ],
+        plugins: [
+            resolve(),
+            babel({
+                exclude: ["node_modules/**"],
+            }),
+        ],
+    },
+];
