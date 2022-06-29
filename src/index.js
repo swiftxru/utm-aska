4a857d4a9f8be85f1899309af828016aa36febbc
Vladimir Banduristov <vaban@norvikbank.ru>
2022-06-29 17:33:14 +0300

    fix: fix some code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as getUtmParams } from "./getUtmParams";
-export { default as getUtmFromUrl } from "./getUtmFromUrl";
-export { default as getCookieByName } from './getCookieByName'
-export { default as getUtmFromCookies } from './getUtmFromCookies'
+export { default as getUtmParams } from "./getUtmParams"
+export { default as getUtmFromUrl } from "./getUtmFromUrl"
+export { default as getCookieByName } from "./getCookieByName"
+export { default as getUtmFromCookies } from "./getUtmFromCookies"
